7b24b09f1495a4470f27d6d1b905f46ac4397791
Anton Savoskin <sairus2k@gmail.com>
2017-05-31 10:02:06 +0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -20,35 +20,41 @@ socket.on('newMessage', (message) => {
 
 socket.on('newLocationMessage', (message) => {
   console.log('newLocationMessage', message);
-  const li = $('<li></li>');
-  const a = $('<a target="_blank">My current location</a>');
-  li.text(`${message.from}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  $('#messages').append(li);
+  const $li = $('<li></li>');
+  const $a = $('<a target="_blank">My current location</a>');
+  $li.text(`${message.from}: `);
+  $a.attr('href', message.url);
+  $li.append($a);
+  $('#messages').append($li);
 });
 
 $('#message-form').on('submit', (e) => {
   e.preventDefault();
+  const $messageInput = $('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
-  }, () => {});
+    text: $messageInput.val()
+  }, () => {
+    $messageInput.val('');
+  });
 });
 
-const locationButton = $('#send-location');
-locationButton.on('click', () => {
+const $locationButton = $('#send-location');
+$locationButton.on('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.')
   }
+  $locationButton.attr('disabled', 'disabled').text('Send location...');
   getPosition()
     .then((position) => {
+    $locationButton.removeAttr('disabled').text('Send location');
       socket.emit('createLocationMessage', {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
     })
     .catch(() => {
+      $locationButton.removeAttr('disabled').text('Send location');
       alert('Unable to fetch location.');
     });
 });
