bef36082656dde85e5ab88b7b4c2aeeacc96e093
Anton Savoskin <sairus2k@gmail.com>
2017-05-31 14:48:48 +0700

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,21 @@ const getPosition = (options) => new Promise((resolve, reject) => {
   navigator.geolocation.getCurrentPosition(resolve, reject, options);
 });
 const formatTime = (time) => moment(time).format('HH:mm');
+const scrollToBottom = () => {
+  const $messages = $('#messages');
+  const $newMessage = $messages.children('li:last-child');
+
+  const clientHeight = $messages.prop('clientHeight');
+  const scrollTop = $messages.prop('scrollTop');
+  const scrollHeight = $messages.prop('scrollHeight');
+
+  const newMessageHeight = $newMessage.innerHeight();
+  const lastMessageHeight = $newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    $messages.scrollTop(scrollHeight);
+  }
+};
 
 socket.on('connect', () => {
   console.log('Connected to server');
@@ -18,12 +33,14 @@ socket.on('newMessage', ({from, text, createdAt}) => {
   const template = $('#message-template').html();
   const html = Mustache.render(template, { from, text, createdAt: formatTime(createdAt) });
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', ({ from, url, createdAt }) => {
   const template = $('#location-message-template').html();
   const html = Mustache.render(template, { from, url, createdAt: formatTime(createdAt) });
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 $('#message-form').on('submit', (e) => {
